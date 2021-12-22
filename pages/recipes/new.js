d5d31bf0615d2820ebb33d6d8c3aec230ab45cd8
James Hardy <jamesthardy@gmail.com>
2021-12-22 15:29:43 +0000

    tweak recipe form

--- pages/recipes/new.js
+++ pages/recipes/new.js
@@ -5,12 +5,13 @@ import { TextField, Typography, FormControl, Grid, Container, IconButton, Icon, 
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
 import { v4 as uuidv4 } from 'uuid';
+import { TimePicker } from '@mui/lab';
 
 export default function Recipes() {
   const router = useRouter();
   const [recipe, setRecipe] = useState({
     title: '',
-    ingredients: ingredients,
+    ingredients: [],
     method: '',
     time: {
       minutes: 0,
@@ -21,7 +22,11 @@ export default function Recipes() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('InputFields', ingredients);
+    const submitRecipe = {
+      ...recipe,
+      ingredients
+    }
+    console.log({submitRecipe});
   };
 
   const handleChange = (e) => {
@@ -53,64 +58,71 @@ export default function Recipes() {
 
   return (
     <Box
-        component='form'
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '100%' },
-        }}
-        noValidate
-        autoComplete='off'
-      >
+      component='form'
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '100%' },
+      }}
+      noValidate
+      autoComplete='off'
+    >
       <Typography variant='h3'>New Recipe</Typography>
       <TextField id='title' label='Title' value={recipe.title} onChange={handleChange} />
       <form className={'root'} onSubmit={handleSubmit}>
-      <Box
-        component='form'
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '20%' },
-        }}
-        noValidate
-        autoComplete='off'
-      >
-        {ingredients.map((inputField) => (
-          <div key={inputField.id}>
-            <TextField
-              name='ingredientName'
-              label='Ingredient'
-              variant='filled'
-              value={inputField.ingredientName}
-              onChange={(event) => handleChangeIngredients(inputField.id, event)}
-            />
-            <TextField
-              name='amount'
-              label='Amount'
-              variant='filled'
-              value={inputField.amount}
-              onChange={(event) => handleChangeIngredients(inputField.id, event)}
-            />
-            <TextField
-              name='measure'
-              label='Measure'
-              variant='filled'
-              value={inputField.measure}
-              onChange={(event) => handleChangeIngredients(inputField.id, event)}
-            />
-            <IconButton disabled={ingredients.length === 1} onClick={() => handleRemoveIngredient(inputField.id)}>
-              <RemoveIcon />
-            </IconButton>
-            <IconButton onClick={handleAddIngredient}>
-              <AddIcon />
-            </IconButton>
-          </div>
-        ))}
-      </Box>
-        <TextField id='method' label='Method' multiline maxRows={99} value={recipe.method} onChange={handleChange} />
-        <Button
-          className={'button'}
-          variant='contained'
-          color='primary'
-          type='submit'
-          onClick={handleSubmit}
+        <Box
+          component='form'
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25%' },
+          }}
+          noValidate
+          autoComplete='off'
         >
+          {ingredients.map((inputField, index) => (
+            <div key={inputField.id}>
+              <TextField
+                name='ingredientName'
+                label='Ingredient'
+                variant='filled'
+                value={inputField.ingredientName}
+                onChange={(event) => handleChangeIngredients(inputField.id, event)}
+              />
+              <TextField
+                name='amount'
+                label='Amount'
+                variant='filled'
+                value={inputField.amount}
+                onChange={(event) => handleChangeIngredients(inputField.id, event)}
+              />
+              <TextField
+                name='measure'
+                label='Measure'
+                variant='filled'
+                value={inputField.measure}
+                onChange={(event) => handleChangeIngredients(inputField.id, event)}
+              />
+              <IconButton disabled={ingredients.length === 1} onClick={() => handleRemoveIngredient(inputField.id)}>
+                <RemoveIcon />
+              </IconButton>
+              {(index + 1) === ingredients.length && (
+                <IconButton onClick={handleAddIngredient}>
+                  <AddIcon />
+                </IconButton>
+              )}
+            </div>
+          ))}
+        </Box>
+        <TextField id='method' label='Method' multiline maxRows={99} value={recipe.method} onChange={handleChange} />
+        <TextField
+          id='time'
+          label='Total time to make'
+          type='time'
+          value={recipe.time}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{ width: 30 }}
+          onChange={handleChange}
+        />
+        <Button className={'button'} variant='contained' color='primary' type='submit' onClick={handleSubmit}>
           Submit
         </Button>
       </form>
